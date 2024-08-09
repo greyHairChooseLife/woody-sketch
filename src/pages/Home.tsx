66726b12f49720a5a3ff6e2dad902049f402e64e
sangyeon kim <greyhairchooselife@gmail.com>
2024-08-09 22:12:51 +0900

    page: other page 생성 및 button으로 page mode조절

---
+++ src/pages/Home.tsx
@@ -0,0 +1,16 @@
+function Home() {
+  return (
+    <>
+      <h1>Woody Sketch</h1>
+      <p className="item_description">
+        우디스케치(Woody sketch)는 목공예를 즐기고 인테리어에 관심 있는 이들을
+        위한 목공 셀프 디자인 플랫폼입니다. 고객들은 인터넷 웹사이트나 휴대폰
+        앱을 통해 자신만의 목공품을 디자인할 수 있으며, 이를 주문 제작할 수
+        있습니다. 이 서비스는 전자상거래와 커스터마이징 서비스를 결합하여 양방향
+        소통 중심의 고객 만족 경영을 추구합니다.
+      </p>
+    </>
+  );
+}
+
+export default Home;
---
+++ src/pages/Other.tsx
@@ -0,0 +1,10 @@
+function Other() {
+  return (
+    <div>
+      <h1>Other</h1>
+      <p>Other 페이지 입니다.</p>
+    </div>
+  );
+}
+
+export default Other;
