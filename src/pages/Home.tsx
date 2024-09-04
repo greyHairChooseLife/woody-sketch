b99b8a46e4e5512d3132129e5b694b03b31c8cc6
sangyeon kim <greyhairchooselife@gmail.com>
2024-09-04 18:06:05 +0900

    style: Home page 첫화면

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -20,9 +20,7 @@ function Home({ setPageMode }: HomeProps) {
   return (
     <>
       <div className="home_layout">
-        <div className="home_section1">
-          <h1>Woody Sketch</h1>
-        </div>
+        <div className="home_section1" />
         <div className="home_section2">
           <BigFrontElement setPageMode={setPageMode} />
           <Designs title="오늘의 추천" elements={todayRecommendObj} />
@@ -41,24 +39,19 @@ type BigFrontElementProps = {
 };
 const BigFrontElement = ({ setPageMode }: BigFrontElementProps) => {
   return (
-    <div className="big_front_element_container">
-      <div className="big_front_element">
-        <div>
-          <div>
-            <div>
-              <div className="big_front_element_top">
-                누구나 쉽게 즐기는 목공예 디자인
-              </div>
-              <div className="big_front_element_mid">WOODY SKETCH</div>
-              <div className="big_front_element_bot">
-                <span>목공 멘토와의 1:1 매칭으로</span>
-                <span>쉽게 배워보세요.</span>
-              </div>
-            </div>
-          </div>
-          <img src="https://cdn.pixabay.com/photo/2016/11/18/17/46/architecture-1836070_960_720.jpg" />
+    <div className="home_page">
+      <div className="home_page_left">
+        <div className="home_word">
+          <div className="home_word_small">새로운 디자인의 시작</div>
+          <div className="home_word_big">Woody Sketch</div>
         </div>
-        <button onClick={() => setPageMode("work")}>스케치 하기</button>
+        <button className="home_button" onClick={() => setPageMode("work")}>
+          Start
+        </button>
+      </div>
+
+      <div className="home_page_right">
+        <img src="https://cdn.pixabay.com/photo/2016/11/18/17/46/architecture-1836070_960_720.jpg" />
       </div>
     </div>
   );
