e6c24b8cbb2d15d2573e2ba30d923e08bc0eb6c1
sangyeon kim <greyhairchooselife@gmail.com>
2024-09-06 01:47:38 +0900

    feat: customer center 컴포넌트의 확장

--- src/components/CustomerCenter.tsx
+++ src/components/CustomerCenter.tsx
@@ -1,12 +1,40 @@
+import { useState } from "react";
 import { TfiHeadphoneAlt } from "react-icons/tfi";
 import "./CustomerCenter.css";
 
 function CustomerCenter() {
+  const [isBoardOpen, setIsBoardOpen] = useState(false);
+
   return (
-    <div className="ct-center">
-      <TfiHeadphoneAlt className="ct-center-icon" />
+    <div>
+      <div
+        className="ct-center"
+        onClick={() => {
+          setIsBoardOpen((prev) => !prev);
+        }}
+      >
+        <TfiHeadphoneAlt className="ct-center-icon" />
+      </div>
+      {isBoardOpen && <MenuBoard />}
     </div>
   );
 }
+
+import { HiOutlineVideoCamera } from "react-icons/hi2";
+
+const MenuBoard = () => {
+  return (
+    <div className="ct-board">
+      <div>
+        <TfiHeadphoneAlt className="ct-board-icon" />
+        <div>1:1 채팅</div>
+      </div>
+      <div>
+        <HiOutlineVideoCamera className="ct-board-icon2" />
+        <div>1:1 통화</div>
+      </div>
+    </div>
+  );
+};
 
 export default CustomerCenter;
